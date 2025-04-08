cc4f4c57c684e7cf78aeccf453e3abd107b9b22b
ilyasbikarnaf <ilyas.bikarnaf@gmail.com>
2025-04-08 14:15:15 +0100

    Added core functionality for a customizable Discord bot that sends scheduled daily messages. Implemented setup with TypeScript, Discord.js, node-cron, and dotenv. Included data.ts with 49 LeetCode challenges and index.ts for bot logic. Configured basic environment variables and build scripts in package.json.

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import "dotenv/config";
+import { CronJob } from "cron";
+import { ChannelType, Client, GatewayIntentBits } from "discord.js";
+import data from "./data";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
+
+client.once("ready", () => {
+  const channel = client.channels.cache.get(process.env.CHANNELID!);
+  let days: number = Number(process.env.FIRSTDAY!);
+
+  if (channel && channel.type === ChannelType.GuildText) {
+    const job = CronJob.from({
+      cronTime: process.env.CRONTIME!,
+      onTick: function () {
+        let challenge = data[days];
+        days++;
+
+        channel.send(
+          `
+          Day ${days}:
+            🧩 Challenge Name: ${challenge.name}
+            💡 Difficulty: ${challenge.difficulty}
+            🔗 Link: ${challenge.link}`
+        );
+        if (days === Number(process.env.LASTDAY!)) {
+          job.stop();
+        }
+      },
+      start: true,
+      timeZone: "America/Los_Angeles",
+    });
+  }
+});
+
+client.login(process.env.TOKEN);
